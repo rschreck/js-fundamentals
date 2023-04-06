410bdd46bab6801d4fb0a47fb7589e43be922dcf
Renuka Schreck <schrer3@gmail.com>
2023-04-06 14:51:10 -0500

    nit

--- advanced/eventloop.js
+++ advanced/eventloop.js
@@ -9,6 +9,6 @@ function main(){
     console.log('C');
 }
 main(); // A,C and B,D
-console.log(0.2+0.1 === 0.3);//false due to flating point precision
+console.log(0.2+0.1 === 0.3);//false due to flatling point precision
 console.log(typeof (0.2+0.1) );
 console.log(typeof 0.3 );
